49de825c332ee552bc276d596533549651113fc2
Ali Schlereth <aschlere@sbcglobal.net>
2017-04-09 20:14:41 -0600

    Create post

--- assets/javascripts/birdeck.js
+++ assets/javascripts/birdeck.js
@@ -10,7 +10,7 @@ var getPosts = function() {
     }
   })
   .fail(function(error){
-    console.log(error)
+    console.error(error)
   })
 }
 
@@ -19,11 +19,41 @@ var fetchPosts = function() {
   getPosts();
 }
 
+var fetchPostById = function() {
+  var postId = $(this).children('.form-control').val();
+  return $.ajax({
+    url: API + '/api/v1/posts/' + postId,
+    method: 'GET'
+  }).done(function(data) {
+    $('#latest-posts').html('');
+    $('#latest-posts').append('<p class="post">' + data.description + '</p>');
+  })
+  .fail(function(error) {
+    console.err(error)
+  });
+}
+
+var createPost = function() {
+  var postDescription = $(this).children('.form-control').val();
+  $('#latest-posts').html('');
+  return $.ajax({
+    url: API + '/api/v1/posts',
+    method: 'POST',
+    data: { 'post': { 'description': postDescription } }
+  }).done(function(data) {
+    $('#latest-posts').append('<p class="post">' + data.description + '</p>');
+  })
+  .fail(function(error) {
+    console.error(error)
+  })
+}
+
 $(document).ready(function(){
   // #tabs for CRUD actions activated
   $( "#tabs" ).tabs();
-  $("#fetch-posts").on('click', fetchPosts)
-
+  $('#fetch-posts').on('click', fetchPosts)
+  $('.show-form').on('submit', fetchPostById)
+  $('.post-form').on('submit', createPost)
   getPosts();
 
 
