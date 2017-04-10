b030305e1b3368c80c3bba8f3dc0f6a301c21bea
Ali Schlereth <aschlere@sbcglobal.net>
2017-04-09 19:41:39 -0600

    Start get all

--- assets/javascripts/birdeck.js
+++ assets/javascripts/birdeck.js
@@ -1,8 +1,24 @@
-var API;
+var API = 'http://localhost:3000'
+
+var getPosts = function() {
+  return $.ajax({
+    url: API + '/api/v1/posts',
+    method: 'GET'
+  }).done(function(data){
+    for (var i = 0; i < data.length; i++ ) {
+      $('#latest-posts').append('<p class="post">' + data[i].description + '</p>');
+    }
+  })
+  .fail(function(error){
+    console.log(error)
+  })
+}
 
 $(document).ready(function(){
   // #tabs for CRUD actions activated
   $( "#tabs" ).tabs();
+  getPosts();
+
 
   // general form submission prevention
   $('form').on('submit', function(event){
