ca94c2ec8133e56a44baa955af81b7082e1dd351
Ali Schlereth <aschlere@sbcglobal.net>
2017-04-09 19:46:32 -0600

    Fetch Post by button

--- assets/javascripts/birdeck.js
+++ assets/javascripts/birdeck.js
@@ -14,9 +14,16 @@ var getPosts = function() {
   })
 }
 
+var fetchPosts = function() {
+  $('#latest-posts').html('');
+  getPosts();
+}
+
 $(document).ready(function(){
   // #tabs for CRUD actions activated
   $( "#tabs" ).tabs();
+  $("#fetch-posts").on('click', fetchPosts)
+
   getPosts();
 
 
