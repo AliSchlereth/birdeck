51519e0e119cfda4b222ad7e0597d8bb7ce69403
Ali Schlereth <aschlere@sbcglobal.net>
2017-04-11 13:17:16 -0600

    Load posts on page refresh

--- assets/javascripts/birdeck.js
+++ assets/javascripts/birdeck.js
@@ -1,4 +1,22 @@
-var API;
+var API = 'http://localhost:3000/api/v1';
+
+var printAllPosts = function(data) {
+  for (var i = 0; i < data.length; i++) {
+    $('#latest-posts').append('<p class="post">' + data[i].description + '<p>' );
+  }
+}
+
+var getAllPosts = function() {
+  return $.ajax({
+    url: API + '/posts',
+    method: 'GET'
+  })
+  .done(printAllPosts)
+  .fail();
+}
+
+getAllPosts();
+
 
 $(document).ready(function(){
   // #tabs for CRUD actions activated
