bb636b1002843826b3b04b38e25c10b3d822aa74
Ali Schlereth <aschlere@sbcglobal.net>
2017-04-11 13:31:42 -0600

    Load post by id

--- assets/javascripts/birdeck.js
+++ assets/javascripts/birdeck.js
@@ -1,5 +1,9 @@
 var API = 'http://localhost:3000/api/v1';
 
+var onFail = function(error) {
+  console.error(error);
+}
+
 var printAllPosts = function(data) {
   for (var i = 0; i < data.length; i++) {
     $('#latest-posts').append('<p class="post">' + data[i].description + '<p>' );
@@ -12,11 +16,28 @@ var getAllPosts = function() {
     method: 'GET'
   })
   .done(printAllPosts)
-  .fail();
+  .fail(onFail);
 }
 
+var printPost = function(data) {
+  clearPosts();
+  $('#latest-posts').append('<p class="post">' + data.description + '</p>')
+}
+
+var fetchPost = function() {
+  var postID = $('input[name=show-id]').val();
+  return $.ajax({
+    url: API + '/posts/' + postID,
+    method: 'GET'
+  })
+  .done(printPost)
+  .fail(onFail);
+}
+
+
+
 var clearPosts = function() {
-  $('#latest-posts').html('')
+  $('#latest-posts').html('');
 }
 
 var fetchPosts = function() {
@@ -26,11 +47,12 @@ var fetchPosts = function() {
 
 getAllPosts();
 
-
 $(document).ready(function(){
   // #tabs for CRUD actions activated
   $( "#tabs" ).tabs();
-  $('button[name=button-fetch]').on('click', fetchPosts)
+  $('button[name=button-fetch]').on('click', fetchPosts);
+  $('.show-form').on('submit', fetchPost);
+
 
   // general form submission prevention
   $('form').on('submit', function(event){
